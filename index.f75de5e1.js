0a35551b794131b6c3e4472b050c6cc29d193896
Valerii Omelnyk <valeraaomelnik@gmail.com>
2024-09-16 00:52:59 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const populationElements = document.querySelectorAll(".population");
+const sumPopulation = [
+    ...populationElements
+].reduce((acc, element)=>{
+    return acc + +element.textContent.replaceAll(",", "");
+}, 0);
+const averagePopulation = sumPopulation / populationElements.length;
+document.querySelector(".total-population").textContent = sumPopulation.toLocaleString("en-US");
+document.querySelector(".average-population").textContent = averagePopulation.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
